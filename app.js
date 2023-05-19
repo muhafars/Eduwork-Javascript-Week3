4960570d842521f52ae3ec771d6c9e99ae258781
muhafash <dev.muhfarash@gmail.com>
2023-05-20 02:14:22 +0700

    membuat checker ganjil genap

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-const kelas12B = {
-  jumlah: 30,
-  ranks: 1,
-  major: "IPS",
-  waliKelas: {
-    nama: "Marlyn Sihaan",
-    umur: 42,
-  },
-  perwakilanKelas: {
-    ketuaKelas: "Muhammad Fajri Assidiq",
-    wakilKetuaKelas: "Nur Aisya",
-  },
-  perlengkapanKelas: ["Spidol", "Penghapus", "Papan Tulis", "Proyektor"],
-  siswaRankings: {
-    1: "Muhammad Fajri Assidiq",
-    2: "Muhammad Alpin Ramadhan",
-  },
+const checkVehiclePlate = function (plateNumber) {
+  // Eliminasi tipe data string dengan regex
+  const plateDigits = plateNumber.match(/\d+/g);
+
+  // Check apakah data berupa string atau number
+  if (plateDigits === null) {
+    return "Invalid";
+  } else {
+    // Mengubah data number namun masih berupa string kedalam tipe data number
+    const convertedDigits = plateDigits.map(digit =>
+      /[13579]$/.test(digit) ? parseInt(digit) : digit
+    );
+
+    // Mengambil nilai akhir dari kumpulan data number
+    const lastDigit = parseInt(convertedDigits[convertedDigits.length - 1]);
+
+    // Check apakah lastDigit berupa ganjil atau genap apalagi bukan tipe data number
+    if (isNaN(lastDigit)) {
+      return "Invalid";
+    } else {
+      const isLastDigitOdd = lastDigit % 2 !== 0;
+      return isLastDigitOdd ? "Ganjil" : "Genap";
+    }
+  }
 };
 
-const {
-  jumlah: anggota, //menginisialisasi dengan nama lainya
-  ranks,
-  major,
-  waliKelas,
-  perwakilanKelas: { ketuaKelas }, // mengambil salah 1 object properties
-  perlengkapanKelas: [benda1, benda2, ...bendaLainya], // bermain dengan destructuring aray
-  ...lainya //mencetak sisanya
-} = kelas12B;
-console.log(anggota, ranks, major, waliKelas, ketuaKelas, benda1, bendaLainya, lainya);
+// Data test dummy
+const plateNumbers = {
+  plate1: "B 2149 SLM",
+  plate2: "D 3732 NKL",
+  plate3: "Z 8001 FGH",
+  plate4: "G 9999 ABC",
+  plate5: "X 1234 XYZ",
+  plate6: "ABCDEF",
+};
+
+for (let plate in plateNumbers) {
+  console.log(`${plate}: ${checkVehiclePlate(plateNumbers[plate])}`);
+}
